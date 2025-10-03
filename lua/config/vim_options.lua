ada559ccd248b5054c490e232cc6d45feb90d283
Akshay-Vs <me.akshay.vs@gmail.com>
2025-10-03 09:46:44 +0530

    feat: periodic sync v-1.0.1

--- lua/config/vim_options.lua
+++ lua/config/vim_options.lua
@@ -8,7 +8,7 @@ vim.opt.relativenumber = true
 vim.g.markdown_recommended_style = 0
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true,
 })
 
 vim.opt.foldmethod = "marker"
@@ -16,9 +16,19 @@ vim.opt.foldmethod = "marker"
 -- Disable LazyVim's automatic plugin ordering
 vim.g.lazyvim_check_order = false
 
+vim.opt.foldenable = false
+
 -- Lua configuration for Neovim
 vim.api.nvim_set_hl(0, "CmpGhostText", {
-    fg = "#585b70",
-    bg = "NONE",
-    italic = true
+	fg = "#585b70",
+	bg = "NONE",
+	italic = true,
+})
+
+-- Enable format on save for all LSP-enabled buffers
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
 })
