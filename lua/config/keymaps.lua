77a7f7c25d753652c1a3ed592c75d4696c7cb8b2
Akshay-Vs <me.akshay.vs@gmail.com>
2025-10-03 09:46:17 +0530

    feat: add inline hint toggle keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 -- Require plugins used in keymaps
 local Snacks_ok, Snacks = pcall(require, "snacks")
 if not Snacks_ok then
-    Snacks = nil
+	Snacks = nil
 end
 -- ================================
 -- Normal Mode Mappings
@@ -15,66 +15,74 @@ end
 
 -- Lspsaga rename
 map("n", "<F2>", "<cmd>Lspsaga rename<CR>", {
-    desc = "Saga Rename"
+	desc = "Saga Rename",
 })
+
+-- format code
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", { noremap = true, silent = true })
 
 -- Go to definition
 map("n", "<leader>oc", vim.lsp.buf.definition, {
-    desc = "Open Component (Go to Definition)"
+	desc = "Open Component (Go to Definition)",
 })
 
 -- Go to next error only
 map("n", "<leader>]", function()
-    vim.diagnostic.goto_next({
-        severity = vim.diagnostic.severity.ERROR
-    })
+	vim.diagnostic.goto_next({
+		severity = vim.diagnostic.severity.ERROR,
+	})
 end, {
-    desc = "Next Error"
+	desc = "Next Error",
 })
 
 -- Go to previous error
 map("n", "<leader>[", function()
-    vim.diagnostic.goto_prev({
-        severity = vim.diagnostic.severity.ERROR
-    })
+	vim.diagnostic.goto_prev({
+		severity = vim.diagnostic.severity.ERROR,
+	})
 end, {
-    desc = "Previous Error"
+	desc = "Previous Error",
 })
 
 -- Move lines (Normal)
 map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", {
-    desc = "Move Down"
+	desc = "Move Down",
 })
 map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", {
-    desc = "Move Up"
+	desc = "Move Up",
 })
+
+-- Toggle inlay_hint
+vim.keymap.set("n", "<leader>h", function()
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "Toggle Inlay Hints" })
 
 -- ================================
 -- Insert Mode Mappings
 -- ================================
 map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", {
-    desc = "Move Down"
+	desc = "Move Down",
 })
 map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", {
-    desc = "Move Up"
+	desc = "Move Up",
 })
 
 -- ================================
 -- Visual Mode Mappings
 -- ================================
 map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", {
-    desc = "Move Down"
+	desc = "Move Down",
 })
 map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", {
-    desc = "Move Up"
+	desc = "Move Up",
 })
 
 -- ================================
 -- Terminal Mode Mappings
 -- ================================
 map("t", "<C-/>", "<cmd>close<cr>", {
-    desc = "Hide Terminal"
+	desc = "Hide Terminal",
 })
 map("t", "<c-_>", "<cmd>close<cr>", {
-    desc = "which_key_ignore"
+	desc = "which_key_ignore",
 })
