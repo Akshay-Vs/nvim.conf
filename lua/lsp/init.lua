a686573bb9de48f0f1e57ae615b658791ae80c3e
Akshay-Vs <me.akshay.vs@gmail.com>
2025-10-28 18:46:01 +0530

    refactor(lsp): remove redundant key mappings and clean up on_attach function

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -63,33 +63,7 @@ M.on_attach = function(client, bufnr)
         buffer = bufnr,
         silent = true
     }
-
-    -- Navigation
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-
-    -- Documentation
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-
-    -- Code actions
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>f", function()
-        vim.lsp.buf.format({
-            async = true
-        })
-    end, opts)
-
-    -- Diagnostics
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-
+    
     -- Workspace
     vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
